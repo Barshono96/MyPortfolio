397b71672acadb88e3143f20d46dfd6e13ee8de0
Shifaeta Kadari Barshon <88356901+Barshono96@users.noreply.github.com>
2025-04-28 16:17:47 +0600

    test0

--- src/app/about/loading.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { useEffect } from "react";
-import { useLoading } from "@/context/loading-provider";
-
-export default function Loading() {
-  // Return null because the loading is already handled by the LoadingProvider
-  return null;
-}
